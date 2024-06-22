ca4f461aff04eae8523b5b45c1303a9faa8b0359
bhuvanksmy <97640981+bhuvanksmy@users.noreply.github.com>
2024-06-22 11:10:18 -0500

    Python CLI application initial commit

---
+++ Loan_app.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\ASB\AppData\Local\Programs\Python\Python311\python.exe""C:\Users\ASB\Desktop\Bhuvana\Perscholas\Capstone_Project\Capstone_CLI\loan_app.py"
+pause
---
+++ Loan_app.py
@@ -0,0 +1,24 @@
+# import required module
+import os
+import sys
+import Datetime
+import re
+
+# help function
+def help():
+    sa = """ Usage:- 
+    $ ./loan_app validate_zipcode   # Validate entered zipcode is in correct format
+    $ ./loan_app validate_monthyear # Validate entered Month & year are in correct format
+    $ ./loan_app ls_trans           # List all the transactions made by customer in specific zipcode for the given month and year
+    $ ./loan_app sort               # Sort the list of transaction in descending order """
+    sys.stdout.buffer.write(sa.encode('utf8'))
+
+    # Validate entered zipcode is in correct format
+    def isvalid_zipcode(z):
+        pattern = r"^\d{5}$"
+        match = re.match(pattern,z)
+        if match: print("Valid Zipcode")
+        else: print("Please enter the valid 5-digit zip code format")
+    
+    # Validate entered Month & year are in correct format
+    def isvalid_monthyear():
