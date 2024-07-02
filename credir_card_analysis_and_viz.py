e16623b1b682da4092ed1bebea2d1d5427d1847a
bhuvanksmy <97640981+bhuvanksmy@users.noreply.github.com>
2024-07-02 07:48:10 -0500

    completed data viz for loan dataset

--- credir_card_analysis_and_viz.py
+++ credir_card_analysis_and_viz.py
@@ -136,9 +136,187 @@ def top10_customers_with_high_transaction_amount():
         conn.close()
         print(str(e))
 
+# 5. Functional Requirements - Data Analysis and Visualization for LOAN Application
+# req - 5.1 Calculate and plot the percentage of applications approved for self-employed applicants
+def plot_percentage_for_applications_approved_for_selfemployed():
+    try:
+        conn =dbconnect.connect(host='localhost',database='creditcard_capstone',user='root',password='password',port='3306')
+        # checking the connection established successfully
+        if conn.is_connected():
+            print('Successfully Connected to MySQL database')
+        mycursor=conn.cursor()
+        query = """select count(Application_Status) as count from cdw_sapp_loan_application where Self_Employed = 'Yes' group by Application_Status"""
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        #print(result)
+        num_rows = int(mycursor.rowcount)
+        print(num_rows)
+        x = map(list, list(result))              # change the type
+        x = sum(x, [])
+        print(x)
+        # Defining the plotsize
+        plt.figure(figsize=(8, 6))
+        
+        # labels for data, replace with your own
+        keys = ['YES', 'NO']
+        
+        # define Seaborn color palette to use 
+        palette_color = sns.color_palette('bright') 
+        
+        # plotting data on chart 
+        plt.pie(x, labels= keys,colors=palette_color,autopct='%.0f%%',startangle=90)
+
+        plt.legend(loc='upper right')
+
+        # Add title to the chart
+        plt.title('Percentage of applications approved for self-employed applicants')
+        
+        # displaying chart 
+        plt.show() 
+        
+    except Exception as e:
+        conn.close()
+        print(str(e))
+
+# req - 5.2 Calculate the percentage of rejection for married male applicants.
+def plot_percentage_of_rejections_for_married_male_applicants():
+    try:
+        conn =dbconnect.connect(host='localhost',database='creditcard_capstone',user='root',password='password',port='3306')
+        # checking the connection established successfully
+        if conn.is_connected():
+            print('Successfully Connected to MySQL database')
+        mycursor=conn.cursor()
+        query = """select count(*) from cdw_sapp_loan_application where Gender = 'Male' AND Married = 'Yes' group by Application_Status"""
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        #print(result)
+        num_rows = int(mycursor.rowcount)
+        print(num_rows)
+        x = map(list, list(result))              
+        x = sum(x, [])
+        print(x)
+        # Defining the plotsize
+        plt.figure(figsize=(8, 6))
+        
+        # labels for data
+        keys = ['Rejected', 'Approved']
+        
+        # define Seaborn color palette to use 
+        palette_color = sns.color_palette('bright') 
+        
+        # plotting data on chart 
+        plt.pie(x, labels= keys,colors=palette_color,autopct='%.0f%%',startangle=90)
+
+        plt.legend(loc='upper right')
+
+        # Add title to the chart
+        plt.title('Percentage of applications approved for self-employed applicants')
+        
+        # displaying chart 
+        plt.show() 
+        
+    except Exception as e:
+        conn.close()
+        print(str(e))
+
+# req 5.3 - Calculate and plot the top three months with the largest volume of transaction data
+
+def top3_months_with_high_transactional_data():
+    try:
+        conn =dbconnect.connect(host='localhost',database='creditcard_capstone',user='root',password='password',port='3306')
+        # checking the connection established successfully
+        if conn.is_connected():
+            print('Successfully Connected to MySQL database')
+        mycursor=conn.cursor()
+        query = """select month(TIMEID),round(sum(TRANSACTION_VALUE)) from cdw_sapp_credit_card group by month(TIMEID) order by round(sum(TRANSACTION_VALUE)) desc limit 3"""
+        mycursor.execute(query)
+        result = mycursor.fetchall(); # fetch all the values from the mysql database
+        #Convert to Pandas Dataframe
+        df = pd.DataFrame(result,columns = ['MONTH','TRANSACTION_VALUE'])
+        #print(df)
+        # Defining the plotsize
+        plt.figure(figsize=(8, 6))
+
+        # Defining the x-axis, the y-axis and the data
+        # from where the values are to be taken
+        palette = ["red","blue","green"]
+        plots = sns.barplot(x=df['MONTH'], y=df['TRANSACTION_VALUE'], data=df,palette=palette,width=0.5)
+        
+        for bar in plots.patches:
+            plots.annotate(format(bar.get_height(), '.0f'), 
+                   (bar.get_x() + bar.get_width() / 2, 
+                    bar.get_height()), ha='center', va='center',
+                   size=15, xytext=(0, 8),
+                   textcoords='offset points')
+        # Title
+        plt.title(" Top 3 months with highest volume of transactional data")
+        # Setting the x-axis label and its size
+        plt.xlabel("MONTH", size=13)
+
+        # Setting the y-axis label and its size
+        plt.ylabel("TRANSACTION_VALUE", size=13)
+
+        # Finally plotting the graph
+        plt.show()
+        mycursor.close() # closing the cursor object connection
+        conn.close()
+        print("Successfully closed the connection")
+    except Exception as e:
+        conn.close()
+        print(str(e))
+
+# req 5.4 - Calculate and plot which branch processed the highest total dollar value of healthcare transactions.
+
+def branch_with_highest_dollarvalue_healthcare_transactions():
+    try:
+        conn =dbconnect.connect(host='localhost',database='creditcard_capstone',user='root',password='password',port='3306')
+        # checking the connection established successfully
+        if conn.is_connected():
+            print('Successfully Connected to MySQL database')
+        mycursor=conn.cursor()
+        query = """ select distinct br.BRANCH_CITY,cr.TRANSACTIONAL_VALUE from cdw_sapp_branch br inner join (select BRANCH_CODE,round(sum(TRANSACTION_VALUE)) as TRANSACTIONAL_VALUE from cdw_sapp_credit_card where TRANSACTION_TYPE = 'Healthcare' group by BRANCH_CODE order by round(sum(TRANSACTION_VALUE)) desc ) cr ON  br.BRANCH_CODE = cr.BRANCH_CODE order by cr.TRANSACTIONAL_VALUE desc limit 10"""
+        mycursor.execute(query)
+        result = mycursor.fetchall(); # fetch all the values from the mysql database
+        #Convert to Pandas Dataframe
+        df = pd.DataFrame(result,columns = ['BRANCH_CITY','TRANSACTION_VALUE'])
+        #print(df)
+        # Defining the plotsize
+        plt.figure(figsize=(8, 6))
+
+        # Defining the x-axis, the y-axis and the data
+        # from where the values are to be taken
+        #palette = ["red","blue","green"]
+        plots = sns.barplot(x=df['BRANCH_CITY'], y=df['TRANSACTION_VALUE'], data=df,width=0.5)
+        
+        for bar in plots.patches:
+            plots.annotate(format(bar.get_height(), '.0f'), 
+                   (bar.get_x() + bar.get_width() / 2, 
+                    bar.get_height()), ha='center', va='center',
+                   size=15, xytext=(0, 8),
+                   textcoords='offset points')
+        # Title
+        plt.title(" Top 3 months with highest volume of transactional data")
+        # Setting the x-axis label and its size
+        plt.xlabel("BRANCH", size=13)
+
+        # Setting the y-axis label and its size
+        plt.ylabel("TRANSACTION_VALUE", size=13)
+
+        # Finally plotting the graph
+        plt.show()
+        mycursor.close() # closing the cursor object connection
+        conn.close()
+        print("Successfully closed the connection")
+    except Exception as e:
+        conn.close()
+        print(str(e))
 
 if __name__=="__main__": 
     print("plotting")
-    plot_highest_transaction_count()
-    top10_States_with_high_customers()
-    top10_customers_with_high_transaction_amount()
+    plot_percentage_for_applications_approved_for_selfemployed()
+    plot_percentage_of_rejections_for_married_male_applicants()
+    top3_months_with_high_transactional_data()
+    branch_with_highest_dollarvalue_healthcare_transactions()
+#     plot_highest_transaction_count()
+#     top10_States_with_high_customers()
+#     top10_customers_with_high_transaction_amount()
